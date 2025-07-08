6f8b762f5b25bbc68993216f54e79f6ed2fe8b21
treshane-ranasinghe <treshaneranasinghe@outlook.com>
2025-07-08 13:58:45 +0530

    added comments

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI() # very important
 
-@app.get("/")
+@app.get("/") # @app = is the decorater
 def read_root():
     return {"message": "Hello, FastAPI"} 
 
 
-@app.get("/about")
-def about():
+@app.get("/about") # none as the path
+def about(): # path operation function 
     return { 'data': {'name':'Treshane'}}
